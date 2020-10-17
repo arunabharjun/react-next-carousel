e935b464aa0e37c68bbef4c2e13be04287829ef2
arunabharjun <sunnyarj114@gmail.com>
2020-10-17 22:40:18 +0530

    Completed the App

--- helpers/api.js
+++ helpers/api.js
@@ -1,5 +1,10 @@
 import data from '../data/imageData.json';
 
+/**
+ * Getting data from local
+ * json file and returning
+ * the images object
+ */
 export const getImageData = () => {
 	return data.images;
 };
